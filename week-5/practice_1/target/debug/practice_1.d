984e0ec6b692d597cae88722c856264a69aba29c
civirterr <icivirter.washima-mohammed@pau.edu.ng>
2022-11-10 19:48:21 +0100

    (My 5th commit)

---
+++ week-5/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-5\practice_1\target\debug\practice_1.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-5\practice_1\src\main.rs
