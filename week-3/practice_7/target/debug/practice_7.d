03c10cdf30c5d6378169b7bff64280bff0c82271
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 12:14:39 +0100

    Delete practice_7.d

--- week-3/practice_7/target/debug/practice_7.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_7\target\debug\practice_7.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_7\src\main.rs
