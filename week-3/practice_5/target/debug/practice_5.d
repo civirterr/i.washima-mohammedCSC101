20db0d0e35dce961ac4f8d45aeb3fae495873574
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 11:32:49 +0100

    Delete practice_5.d

--- week-3/practice_5/target/debug/practice_5.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_5\src\main.rs
