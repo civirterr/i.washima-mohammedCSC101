1a444d46545fb356d6bfe5f1e8863a59f74f072f
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 10:56:55 +0100

    Delete practice_10.d

--- week-3/practice_10/target/debug/practice_10.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_10\src\main.rs
