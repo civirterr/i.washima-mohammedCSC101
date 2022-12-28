2dbc988723414f6362cc1c1cd6f96d65b2276843
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 11:41:09 +0100

    Delete practice_6.d

--- week-3/practice_6/target/debug/deps/practice_6.d
+++
@@ -1,5 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_6\target\debug\deps\practice_6.exe: src\main.rs
-
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_6\target\debug\deps\practice_6.d: src\main.rs
-
-src\main.rs:
