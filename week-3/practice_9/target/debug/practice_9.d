53c244a24967e0410dcdcafe742a43dbfe9166c6
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 16:51:15 +0100

    Delete practice_9.d

--- week-3/practice_9/target/debug/practice_9.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_9\src\main.rs
