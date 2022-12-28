20a8e2a348387e727d4670e078b323237cccac21
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 12:29:22 +0100

    Delete practice_8.d

--- week-3/practice_8/target/debug/deps/practice_8.d
+++
@@ -1,5 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_8\target\debug\deps\practice_8.exe: src\main.rs
-
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_8\target\debug\deps\practice_8.d: src\main.rs
-
-src\main.rs:
