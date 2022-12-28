1ab446d4831e8c65b423217b1b4f10c95262f4db
civirterr <115739213+civirterr@users.noreply.github.com>
2022-12-28 13:00:24 +0100

    Delete practice_8.d

--- week-3/practice_8/target/debug/practice_8.d
+++
@@ -1,1 +0,0 @@
-C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\IcivirTer\Documents\i.washima-mohammedCSC101\week-3\practice_8\src\main.rs
